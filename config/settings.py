30e2bb2acc631317fef98ac3e42b35a0317edff2
myka <aleshin.tomsk2@mail.ru>
2025-05-22 19:19:48 +0700

    config aded

---
+++ config/settings.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env"
+    )
+
+    API_TOKEN: str
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    DB_URL: str
+
+
+    @property
+    def dB_url(self) -> str:
+        return self.DB_URL.format(
+            self.DB_USER,
+            self.DB_PASS,
+            self.DB_HOST,
+            self.DB_PORT,
+            self.DB_NAME,
+        )
+    
+settings = Settings()
+
+
