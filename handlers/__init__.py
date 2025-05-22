4e5e5b16c5bb121d8e8e394d3ace8b7cec6d0913
myka <aleshin.tomsk2@mail.ru>
2025-05-22 21:28:32 +0700

    added registration with command /start and provide_location button

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router
-from . import commands
+from . import commands, weather
 
 root_router = Router()
 
 root_router.include_router(commands.router)
+root_router.include_router(weather.router)
