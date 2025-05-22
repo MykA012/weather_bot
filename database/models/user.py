65fe88350a9556d1fff0345cf6aa9c64ec0e740e
myka <aleshin.tomsk2@mail.ru>
2025-05-22 19:47:50 +0700

    added user model

---
+++ database/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import BigInteger, String, func
+
+from datetime import datetime
+
+from database.base import Base
+
+
+class User(Base):
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
+    username: Mapped[str | None]
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str | None]
+
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    city: Mapped[str | None]
