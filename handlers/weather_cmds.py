2a89f938706eb6f05b66256de884a270574a540e
myka <aleshin.tomsk2@mail.ru>
2025-05-27 20:14:48 +0700

    city validate added

--- handlers/weather_cmds.py
+++ handlers/weather_cmds.py
@@ -14,7 +14,7 @@ async def weather_now(message: Message):
         user = await user_repo.get_by_telegram_id(message.from_user.id)
 
     try:
-        weather = weather_api.now(
+        weather = weather_api.today(
             city=user.city, latitude=user.latitude, longitude=user.longitude
         )
     except HTTPError:
@@ -25,6 +25,28 @@ async def weather_now(message: Message):
         f"""
 {weather["icon"]} Прогноз на сегодня ({weather["name"]})
 {weather["description"]}
-Температура: {weather["temp"]}
-Ощущается как {weather["feels_like"]}"""
+Температура: {weather["temp"]}°C
+Ощущается как {weather["feels_like"]}°C"""
+    )
+
+
+@router.message(F.text == "Погода завтра")
+async def weather_tomorrow(message: Message):
+    async with get_user_repo() as user_repo:
+        user = await user_repo.get_by_telegram_id(message.from_user.id)
+
+    try:
+        weather = weather_api.tomorrow(
+            city=user.city, latitude=user.latitude, longitude=user.longitude
+        )
+    except HTTPError:
+        await message.answer("Что то пошло не так (")
+        return
+
+    await message.answer(
+        f"""
+{weather["icon"]} Прогноз на завтра ({weather["name"]})
+{weather["description"]}
+Температура: {weather["temp"]}°C
+Ощущается как {weather["feels_like"]}°C"""
     )
--- services/sync/weather_api.py
+++ services/sync/weather_api.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import requests
 
 from config.settings import settings
@@ -13,17 +15,16 @@ def validate_city(city: str) -> bool:
     return response.status_code == 200
 
 
-def now(
+def today(
     city: str = None, latitude: float = None, longitude: float = None
 ) -> dict[str:str]:
     params = {
-        "units": "metric",
         "q": city,
         "lat": latitude,
         "lon": longitude,
-        "exclude": "current",
-        "appid": settings.WEATHER_APIKEY,
+        "units": "metric",
         "lang": "ru",
+        "appid": settings.WEATHER_APIKEY,
     }
     response = requests.get(
         url="https://api.openweathermap.org/data/2.5/weather", params=params
@@ -34,14 +35,53 @@ def now(
 
     icon = ids[response["weather"][0]["id"]]["emoji"]
     name = response["name"]
+    description = response["weather"][0]["description"]
     temp = response["main"]["temp"]
     feels_like = response["main"]["feels_like"]
-    description = response["weather"][0]["description"]
 
     return {
         "icon": icon,
         "name": name,
+        "description": description,
         "temp": temp,
         "feels_like": feels_like,
+    }
+
+
+def tomorrow(
+    city: str = None, latitude: float = None, longitude: float = None
+) -> dict[str:str]:
+    params = {
+        "q": city,
+        "lat": latitude,
+        "lon": longitude,
+        "units": "metric",
+        "lang": "ru",
+        "appid": settings.WEATHER_APIKEY,
+    }
+    response = requests.get(
+        url="https://api.openweathermap.org/data/2.5/forecast", params=params
+    ).json()
+
+    if response["cod"] != "200":
+        raise requests.HTTPError
+
+    tomorrow_date = (
+        f"{(datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")} 12:00:00"
+    )
+
+    forecast = [item for item in response["list"] if item["dt_txt"] == tomorrow_date][0]
+
+    icon = ids[forecast["weather"][0]["id"]]["emoji"]
+    name = response["city"]["name"]
+    description = forecast["weather"][0]["description"]
+    temp = forecast["main"]["temp"]
+    feels_like = forecast["main"]["feels_like"]
+
+    return {
+        "icon": icon,
+        "name": name,
         "description": description,
+        "temp": temp,
+        "feels_like": feels_like,
     }
