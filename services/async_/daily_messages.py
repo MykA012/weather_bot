c72bbb9f889fa63cdc5ac429b016b9db0da97927
MykA012 <aleshin.tomsk2@mail.ru>
2025-06-02 00:11:47 +0700

    daily notifications added

--- services/async_/daily_messages.py
+++ services/async_/daily_messages.py
@@ -13,31 +13,31 @@ async def send_daily_messages(bot: Bot):
     while True:
         now = datetime.now().time()
 
-        # if now.hour == time.hour and now.minute == target_time.minute:
-        async with get_user_repo() as user_repo:
-            users = await user_repo.get_all_users()
+        if now.hour == time.hour and now.minute == target_time.minute:
+            async with get_user_repo() as user_repo:
+                users = await user_repo.get_all_users()
 
-        for user in users:
-            if user.notifications:
-                try:
-                    weather = weather_api.today(
-                        city=user.city,
-                        latitude=user.latitude,
-                        longitude=user.longitude,
-                    )
+            for user in users:
+                if user.notifications:
+                    try:
+                        weather = weather_api.today(
+                            city=user.city,
+                            latitude=user.latitude,
+                            longitude=user.longitude,
+                        )
 
-                    message = f"""
+                        message = f"""
 {weather["icon"]} Прогноз на сегодня ({weather["name"]})
 {weather["description"]}
 Температура: {weather["temp"]}°C
 Ощущается как {weather["feels_like"]}°C"""
 
-                    await bot.send_message(user.telegram_id, message)
-                    logging.info(f"Message has been sended to user: id = {user.id}")
+                        await bot.send_message(user.telegram_id, message)
+                        logging.info(f"Message has been sended to user: id = {user.id}")
 
-                except Exception as e:
-                    logging.info(
-                        f"Error while senfing message user: id = {user.id}: {e}"
-                    )
+                    except Exception as e:
+                        logging.info(
+                            f"Error while senfing message user: id = {user.id}: {e}"
+                        )
 
         await asyncio.sleep(5)
